b695453bec0547ba1ab8ac2a58e9a5905e727e01
Jhonny Michel <jhonnymichel@outlook.com>
2019-10-11 09:02:35 -0300

    correctly ignoring stuff

---
+++ src/config.example.js
@@ -0,0 +1,8 @@
+// duplicate this file into 'config.js', fill the information
+
+const CALENDAR_CONFIG = {
+  apiKey: 'YOUR_API_KEY',
+  calendarId: 'YOUR_CALENDAR_ID0@group.calendar.google.com',
+};
+
+export {CALENDAR_CONFIG};
--- src/config.js
+++
@@ -1,8 +0,0 @@
-const CALENDAR_CONFIG = {
-  apiKey: 'AIzaSyCg6TVnwBbs6IITPFErEZ1SEL9_DU1gYls',
-  calendarId: 'python.org.br_m7e5tbnbdkajrere5kd1sue2b0@group.calendar.google.com',
-};
-
-export {
-  CALENDAR_CONFIG
-}
